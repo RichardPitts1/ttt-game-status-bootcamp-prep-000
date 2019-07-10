a8554abc26ca43f5192aba7858c7959b43ad5f72
Richard Pitts <richardepitts@gmail.com>
2019-07-10 03:36:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,24 +15,7 @@ WIN_COMBINATIONS = [
   [6, 4, 2]
 ]
 
-def won?(board)
-  WIN_COMBINATIONS.each {|win_combo|
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
 
-  position_1 = board[win_index_1] # load the value of the board at win_index_1
-  position_2 = board[win_index_2] # load the value of the board at win_index_2
-  position_3 = board[win_index_3] # load the value of the board at win_index_3
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
-    end
-  }
-  return false
-end
 
 def full?(board)
   board.all? {|index| index == "X" || index == "O"}
